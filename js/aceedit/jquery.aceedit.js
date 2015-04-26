97d92a9f517ede87d9a93181d6fc359c9b5a8df2
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2015-04-27 00:43:15 +0200

    elkanatooltip

---
+++ js/aceedit/jquery.aceedit.js
@@ -0,0 +1,68 @@
+/**
+ * Zależne od underscore.js, jQuery
+ */
+;(function ($) {
+    /**
+     * http://ace.ajax.org/api/edit_session.html
+     * g(clearSelection site:ace.ajax.org)
+     */
+    $.fn.aceedit = function () {      
+        var bag = [];
+        $(this).each(function () {
+            var that, clone, script, editor, div, t = '', d, newh = 0;
+            that = $(this);
+
+            d = that.data('h')
+            d && that.css({height:d});
+            d = that.data('w')
+            d && that.css({width:d});
+
+            script = that.children('script');
+            if (script.length) {
+                t = script.html();                
+            }
+            else {
+                script = that.children('textarea');
+                t = script.html(); 
+                log(t)
+            }
+            script.remove();
+
+            div = that.clone().removeClass('editor').removeClass('syntax').appendTo(that);
+
+            // dodaję też clean:both jako następny element
+            $('<div></div>').attr('style','clear:both;').appendTo(that);
+
+            editor = ace.edit(div.get(0));
+            editor.getSession().setTabSize(4);
+            editor.setTheme("ace/theme/idle_fingers");
+            editor.getSession().setUseWrapMode(true);
+
+            d = that.data('lang');
+            (d == 'js') && (d = 'javascript');
+            d && editor.getSession().setMode("ace/mode/"+d);
+
+            that.hasClass('syntax') && editor.setReadOnly(true);  // false to make it editable
+      //        editor.getSession().setMode("ace/mode/javascript");
+            editor.setValue(_.unescape(t)); 
+            editor.clearSelection();
+            if (!that.data('h')) {
+      //        newh = ((editor.getSession().getDocument().getLength()+1) * editor.renderer.lineHeight) + 
+      //               editor.renderer.scrollBar.getWidth();
+                setTimeout(function () {
+                    newh = div.find('.ace_sb > div').height();
+                    div.add(that).height(newh);
+                    editor.resize();
+                },10);
+            }
+            editor.resize();
+            editor.$ = that;
+            that.data('editor',editor);
+            bag.push(editor);
+        });
+        return bag.length == 1 ? bag[0] : bag;      
+    };
+    $.aceedit = function () {
+        $('.editor, .syntax').aceedit();
+    };
+})(jQuery);
