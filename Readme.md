c829a9794bf6e28e95277fffcf76f951f3067597
Szymon Dzialowski <sd@phase-ii.com>
2018-06-15 23:39:51 +0100

    ace editor

--- Readme.md
+++ Readme.md
@@ -1,3 +1,17 @@
+
+# Ace editor objects syntax:
+
+    <div class="editor|syntax" data-lang="js" data-w="95%">
+        <textarea type="editor">
+            function (a, b, c) {
+                console.log('textarea', a)
+            }
+        </textarea>
+    </div>
+    
+[supported syntax](https://github.com/ajaxorg/ace/blob/master/lib/ace/ext/modelist.js#L53)    
+# other   
+
 http://httpd.pl/stopsopa.github.io/index.html
 http://httpd.pl/stopsopa.github.io/demos/jquery.elkanatooltip/demo.html
 http://httpd.pl/dropdown
--- js/github.js
+++ js/github.js
@@ -1,3 +1,26 @@
+
+var manipulation = {
+    after: function (referenceNode, newNode) {
+        return this.before(referenceNode.nextSibling, newNode);
+    },
+    before: function (referenceNode, newNode) {
+        referenceNode.parentNode.insertBefore(newNode, referenceNode);
+        return this;
+    },
+    append: function (parentNode, newNode) {
+        parentNode.appendChild(newNode);
+        return this;
+    },
+    prepend: function (parentNode, newNode) {
+        parentNode.insertBefore(newNode, parentNode.firstChild);
+        return this;
+    },
+    remove: function (node) {
+        node.parentNode.removeChild(node);
+        return this;
+    }
+};
+
 (function () {
 
     const log = (function () {
@@ -10,28 +33,6 @@
     }());
 
     window.log = log;
-
-    var manipulation = {
-        after: function (referenceNode, newNode) {
-            return this.before(referenceNode.nextSibling, newNode);
-        },
-        before: function (referenceNode, newNode) {
-            referenceNode.parentNode.insertBefore(newNode, referenceNode);
-            return this;
-        },
-        append: function (parentNode, newNode) {
-            parentNode.appendChild(newNode);
-            return this;
-        },
-        prepend: function (parentNode, newNode) {
-            parentNode.insertBefore(newNode, parentNode.firstChild);
-            return this;
-        },
-        remove: function (node) {
-            node.parentNode.removeChild(node);
-            return this;
-        }
-    };
 
     window.manipulation = manipulation;
 
@@ -175,6 +176,7 @@
     [
         '/css/normalize.css',
         '/css/main.css',
+        '/js/aceedit/jquery.aceedit.css',
         '//fonts.googleapis.com/css?family=Open+Sans:300,400',
     ].forEach(u => {
 
@@ -224,17 +226,153 @@
         '/js/polyfill.js',
         '/js/permalink-my.js',
         '/js/domcontentloaded.js',
+        '/js/lodash-4.17.10.js',
     ].forEach(u => {
 
         // https://stackoverflow.com/a/524721
         var head = document.head || document.getElementsByTagName('head')[0],
-            style = document.createElement('script');
+            script = document.createElement('script');
 
-        style.type = "text/javascript";
-        style.async = false;
+        // script.async = false;
 
-        style.setAttribute('src', u);
+        script.setAttribute('src', u);
 
-        head.appendChild(style);
+        head.appendChild(script);
     })
+}());
+
+// ace editor
+(function () {
+
+    var p;
+
+    window.doace = function () {
+
+        if (!p) {
+
+            p = new Promise(function (resolve) {
+                (function run() {
+                    if (window._) {
+                        resolve()
+                    }
+                    else {
+                        setTimeout(run, 100);
+                    }
+                }());
+            });
+        }
+
+        p.then(function () {
+            Array.prototype.slice.call(document.querySelectorAll('.editor, .syntax')).forEach(function (el) {
+
+                if (el.classList.contains('handled')) {
+
+                    log('ace - handled')
+
+                    return true;
+                }
+
+                var script, editor, div, t = '', d;
+
+                script = el.querySelector('script, textarea');
+
+                d = el.dataset.h;
+                d && (el.style.height = d);
+                d = el.dataset.w;
+                d && (el.style.width = d);
+
+                if (!script) {
+
+                    log('ace - no script child found');
+
+                    return true;
+                }
+
+                t = script.innerHTML;
+
+                manipulation.remove(script);
+
+                div = el.cloneNode(false);
+
+                el.classList.add('handled');
+
+                manipulation.append(el, div);
+
+                div.classList.remove('editor')
+                div.classList.remove('syntax')
+
+                const clear = document.createElement('div');
+
+                clear.style.clear = 'both';
+
+                manipulation.append(el, clear);
+
+                // manipulation.after(el, clear.cloneNode(false))
+
+                editor = ace.edit(div);
+
+                window.ed = editor
+
+                editor.getSession().setTabSize(4);
+                editor.setTheme("ace/theme/idle_fingers");
+                editor.getSession().setUseWrapMode(true);
+
+                d = el.dataset.lang;
+                (d == 'js') && (d = 'javascript');
+                d && editor.getSession().setMode("ace/mode/"+d);
+
+                el.classList.contains('syntax') && editor.setReadOnly(true);  // false to make it editable
+                //        editor.getSession().setMode("ace/mode/javascript");
+                editor.setValue(_.unescape(t).replace(/^ *\n([\s\S]*?)\n *$/g, '$1'));
+                // editor.setValue(t);
+                editor.clearSelection();
+                // editor.setOptions({
+                //     maxLines: Infinity
+                // });
+
+
+                var heightUpdateFunction = function() {
+
+                    // http://stackoverflow.com/questions/11584061/
+                    var newHeight =
+                        editor.getSession().getScreenLength()
+                        * editor.renderer.lineHeight
+                        + editor.renderer.scrollBar.getWidth();
+
+                    log('new height', newHeight);
+
+                    var h = newHeight.toString();
+
+                    // h += 1000;
+
+                    h += 'px';
+
+                    div.style.height = h;
+
+                    // Array.prototype.slice.call(document.querySelector('.editor').querySelectorAll('[class]'))
+                    //     .map(e => e.style.height = h)
+                    // ;
+                    // $('#editor').height(newHeight.toString() + "px");
+                    // $('#editor-section').height(newHeight.toString() + "px");
+
+                    // This call is required for the editor to fix all of
+                    // its inner structure for adapting to a change in size
+                    editor.resize();
+                };
+
+                // Set initial size to match initial content
+                heightUpdateFunction();
+
+                // Whenever a change happens inside the ACE editor, update
+                // the size again
+                editor.getSession().on('change', heightUpdateFunction);
+
+            })
+        });
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        window.doace();
+    })
+
 }());
