b4ded0573064682a3640a91ff446da145a0c3c4a
stopsopa <grytysek@gmail.com>
2020-04-16 23:30:15 +0100

    jira

---
+++ pages/bookmarklets/compress.sh
@@ -0,0 +1,17 @@
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+
+ROOT="$DIR/../../"
+
+set -e
+set -x
+
+if [ ! -e "$ROOT/node_modules" ]; then
+
+    (cd "$ROOT" && yarn)
+fi
+
+node "$ROOT/node_modules/.bin/uglifyjs" "$DIR/jira-create.js" -o "$DIR/jira-create.min.js" -m -c toplevel,sequences=false --mangle-props
+
+node "$DIR/template.js"
