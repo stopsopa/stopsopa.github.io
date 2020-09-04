700f6d5f1373a9bedb796e6ea55b3d80c5dc0ef3
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2020-09-04 20:58:10 +0100

    webpack + react

---
+++ roderic/webpack/restrict-to-node.js
@@ -0,0 +1,26 @@
+/**
+ * use:
+ *
+ * require('roderic/webpack/restrict-to-node')(__filename);
+ */
+
+module.exports = file => {
+
+    if ( ! file ) {
+
+        throw `restrict-to-node: file parameter not specified`;
+    }
+
+    const node = typeof global !== 'undefined' && Object.prototype.toString.call(global.process) === '[object process]';
+
+    if ( ! node ) {
+
+        const msg = `\n\n\n${file} is included in web bundle\n\n\n`;
+
+        window.addEventListener('DOMContentLoaded', () => setTimeout(() => document.write(msg), 500))
+
+        alert(msg);
+
+        throw msg;
+    }
+}
