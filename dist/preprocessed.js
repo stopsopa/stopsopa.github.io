0cb118d924868bf9e3baebee1e676379d8b44979
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2020-09-08 23:37:13 +0100

    FIREBASE_

--- dist/preprocessed.js
+++ dist/preprocessed.js
@@ -39,14 +39,14 @@ window.env = (function (e) {
 }({
     "PROJECT_NAME": "stopsopatools",
     "NODE_PORT": "7898",
-    "API_KEY": "AIzaSyBwjXbJQTXj258mLFMcHswgy6FgKHVHMLs",
-    "AUTH_DOMAIN": "github-ae2af.firebaseapp.com",
-    "DATABASE_URL": "https://github-ae2af.firebaseio.com",
-    "PROJECT_ID": "github-ae2af",
-    "STORAGE_BUCKET": "github-ae2af.appspot.com",
-    "MESSAGING_SENDER_ID": "496172961972",
-    "API_ID": "1:496172961972:web:c9363e230fede3127a07e1",
-    "MEASUREMENT_ID": "G-F78BC9VYQ5"
+    "FIREBASE_API_KEY": "AIzaSyBwjXbJQTXj258mLFMcHswgy6FgKHVHMLs",
+    "FIREBASE_AUTH_DOMAIN": "github-ae2af.firebaseapp.com",
+    "FIREBASE_DATABASE_URL": "https://github-ae2af.firebaseio.com",
+    "FIREBASE_PROJECT_ID": "github-ae2af",
+    "FIREBASE_STORAGE_BUCKET": "github-ae2af.appspot.com",
+    "FIREBASE_MESSAGING_SENDER_ID": "496172961972",
+    "FIREBASE_API_ID": "1:496172961972:web:c9363e230fede3127a07e1",
+    "FIREBASE_MEASUREMENT_ID": "G-F78BC9VYQ5"
 }))
 
 log("const env = window.env;")
