47789b2d4de190e2881d156d8a97bdff784d3154
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2020-10-15 16:47:40 +0100

    gitstorage-config

---
+++ gitstorage-config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+GITSTORAGESOURCE="git@github.com:stopsopa/gitstorage.git"
+
+GITSTORAGELIST=(
+    '.env::bitbucket-stopsopa.github.io/.env'
+    'gitstorage-config.sh::github-stopsopa.github.io/gitstorage-config.sh'
+)
+
