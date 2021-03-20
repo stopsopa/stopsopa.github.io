246731c1f59f6e48a26e7d09cfad1e8caa6e8b2f
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2021-03-20 00:06:38 +0000

    update

--- pages/portsregistry/portsregistry.entry.jsx
+++ pages/portsregistry/portsregistry.entry.jsx
@@ -5,6 +5,10 @@ import { render } from 'react-dom';
 
 import log from 'inspc';
 
+import nget from 'nlab/get';
+
+import nset from 'nlab/set';
+
 import useCustomState from '../useCustomState'
 
 const section = 'portsregistry';
@@ -13,9 +17,21 @@ import isObject from 'nlab/isObject';
 
 const Main = () => {
 
-  const [ groupslist, setGroupslist ] = useState(false);
+  const [ ports, setPortsList ] = useState(false);
 
-  const [ groupname, setGroupnameRaw ] = useState('');
+  const defaultInput = () => ({
+    port: '',
+    description: ''
+  });
+
+  const [ input, setInputRaw ] = useState(defaultInput());
+
+  const [ expanded, setExpanded ] = useState([]);
+
+  const setInput = (key, value) => {
+
+    setInputRaw(nset({...input}, key, value));
+  }
 
   const {
     error,
@@ -28,18 +44,18 @@ const Main = () => {
     section,
   });
 
-  const refreshGroupList = async function () {
+  const refreshPortsList = async function () {
 
-    let list = await get(`groupslist`);
+    let list = await get(`ports`);
 
-    setGroupslist(list);
+    setPortsList(list);
   };
 
   useEffect(() => {
 
     if (id) {
 
-      refreshGroupList();
+      refreshPortsList();
     }
 
   }, [id]);
@@ -56,78 +72,123 @@ const Main = () => {
     return <div>Connecting to custom state...</div>
   }
 
-  const setGroupname = name => {
+  const submit = async () => {
 
-    if (typeof name === 'string') {
+    const foundNegative = Object.values(input).find(v => !Boolean(v)) !== undefined;
 
-      name = name.trim();
+    const save = ! foundNegative;
+
+    if (save && /^\d+$/.test(input.port)) {
+
+      if ( Object.values(ports || {}).find(o => o.port === input.port) ) {
+
+        return alert(`port '${input.port}' is already registered`);
+      }
+
+      input.created_at = (new Date()).toISOString().substring(0, 19).replace('T', ' ')
+
+      await push({
+        key: `ports`,
+        data: input,
+      });
+
+      setInputRaw(defaultInput());
+
+      await refreshPortsList();
     }
-
-    setGroupnameRaw(name);
   }
 
   return (
     <div>
-      <h4>Add group:</h4>
+      <h4>Add port:</h4>
       <form onSubmit={async e => {
 
         e.preventDefault();
 
-        let list = [];
-
-        if (isObject(groupslist)) {
-
-          list = Object.keys(groupslist).map(key => groupslist[key].groupname.toLowerCase());
-        }
-
-        if (groupname) {
-
-          if ( list.includes(groupname.toLowerCase()) ) {
-
-            return alert(`group by name '${groupname}' already exist`);
-          }
-
-          await push({
-            key: `groupslist`,
-            data: {
-              groupname,
-            },
-          });
-
-          setGroupname('');
-
-          await refreshGroupList();
-        }
-
+        submit();
       }}>
-        <label>
-          name:
-          <input type="text" value={groupname} onChange={e => setGroupname(e.target.value)}/> {groupname}
-        </label>
+        <table>
+          <tbody>
+          <tr>
+            <td>
+              port:
+            </td>
+            <td>
+              <input type="text" value={input.port || ''} onChange={e => setInput('port', e.target.value)}/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              description:
+            </td>
+            <td>
+              <textarea value={input.description || ""} onChange={e => setInput('description', e.target.value)}/>
+            </td>
+          </tr>
+          </tbody>
+        </table>
         <br/>
         <button type="submit">add</button>
       </form>
 
       <br/>
+      <table className="list" border="0">
+        <tbody>
 
-      groups:
-      <br/>
-      {/*<pre>{JSON.stringify(groupslist, null, 4)}</pre>*/}
-      <ul>
-        {isObject(groupslist) && Object.keys(groupslist).map(key => {
+          {isObject(ports) && (function (list) {
 
-          const g = groupslist[key];
+            list.sort((a, b) => {
 
-          return (
-            <li key={key} data-key={key}>{g.groupname} <button onClick={async e => {
+              if (a.created_at === b.created_at) {
 
-              await del(`groupslist/${key}`);
+                return 0;
+              }
+              return a.created_at > b.created_at ? 1 : -1;
+            });
 
-              await refreshGroupList();
-            }}>del</button></li>
-          )
-        })}
-      </ul>
+            return list.map(g => (
+              <tr key={g.key} data-key={g.key}>
+                <td valign="top">
+                  <span className="date">{g.created_at}</span>
+                </td>
+                <td valign="top">
+                  <span className="port">{g.port}</span>
+                </td>
+                <td valign="top">
+                  <button onClick={async e => {
+
+                    if (confirm("Delete?")) {
+
+                      await del(`ports/${g.key}`);
+
+                      await refreshPortsList();
+                    }
+
+                  }}>del</button>
+                </td>
+                {(function (d) {
+                  if (d.length > 50 && !expanded.includes(g.key)) {
+
+                    return <td>{d.substring(0, 50)} <button onClick={() => setExpanded((function (d) {
+                      d.push(g.key);
+                      return d;
+                    }([...expanded])))}>...</button></td>
+                  }
+                  return <td>{d}</td>
+                }(g.description))}
+              </tr>
+            ))
+          }(Object.entries(ports).reduce((acc, [key, value]) => {
+
+            acc.push({
+              key,
+              ...value,
+            });
+
+            return acc;
+          }, [])))}
+        </tbody>
+      </table>
 
       <hr/>
 
