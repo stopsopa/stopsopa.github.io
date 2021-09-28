d818ad7678983dc354bcbb89e90606bd8a107759
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2021-09-28 15:05:46 +0100

    ps $$ | sed -rn "s/.*[-\/]+(bash|z?sh).*/\1/p"

--- bash/git/hardlink.sh
+++ bash/git/hardlink.sh
@@ -18,7 +18,7 @@ function yellow {
     printf "\e[33m$1\e[0m"
 }
 
-_SHELL="$(ps -p $$ | sed -rn "s/.*[-\/]+(bash|zsh).*/\1/p")"; # zsh || bash
+_SHELL="$(ps $$ | sed -rn "s/.*[-\/]+(bash|z?sh).*/\1/p")"; # zsh || bash
 
 if [ "${_SHELL}" = "zsh" ]; then
 
