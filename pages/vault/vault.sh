5cfa94970dfe7ae8a48b5cea00efe6d61acf8d71
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2021-09-28 16:08:48 +0100

    <

--- pages/vault/vault.sh
+++ pages/vault/vault.sh
@@ -2,13 +2,13 @@
 _SHELL="$(ps "${$}" | grep "${$} " | grep -v grep | sed -rn "s/.*[-\/]+(bash|z?sh).*/\1/p")"; # bash || sh || zsh
 case ${_SHELL} in
   zsh)
-    _DIR="$( cd "$( dirname "${(%):-%N}" )" && pwd -P )<"
+    _DIR="$( cd "$( dirname "${(%):-%N}" )" && pwd -P )"
     ;;
   sh)
-    _DIR="$( cd "$( dirname "${0}" )" && pwd -P )<"
+    _DIR="$( cd "$( dirname "${0}" )" && pwd -P )"
     ;;
   *)
-    _DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )<"
+    _DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
     ;;
 esac
 
