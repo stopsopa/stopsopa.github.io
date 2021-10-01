b2c3ebd3626653706828a1dd21042564bf8650b1
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2021-10-01 01:18:32 +0100

    bash scripts

--- bash/env.sh
+++ bash/env.sh
@@ -2,22 +2,32 @@
 # call with one argument, script will return value of environment variable from .env under ${1} name variable
 # /bin/bash bash/env.sh ../.env PROTECTED_MYSQL_HOST
 
-
-_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+_SHELL="$(ps "${$}" | grep "${$} " | grep -v grep | sed -rn "s/.*[-\/]+(bash|z?sh) .*/\1/p")"; # bash || sh || zsh
+case ${_SHELL} in
+  zsh)
+    _DIR="$( cd "$( dirname "${(%):-%N}" )" && pwd -P )"
+    ;;
+  sh)
+    _DIR="$( cd "$( dirname "${0}" )" && pwd -P )"
+    ;;
+  *)
+    _DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+    ;;
+esac
 
 source "${_DIR}/colours.sh";
 
 if [ ${#} -lt 2 ]; then
 
-    echo "bash/env.sh: not enough arguments given";
+    { red "bash/env.sh: not enough arguments given"; } 2>&3
 
     exit 1
 fi
 
-if [ ! -e "${1}" ]; then  # not exist (fnode, directory, socket, etc.)
+if [ ! -f "${1}" ]; then  # not exist (fnode, directory, socket, etc.)
 
     # https://stackoverflow.com/a/23622446
-    { red ".env file ${1} file doesn't exist"; } 2>&3
+    { red "under path ${1} there is no file"; } 2>&3
 
     exit 1;
 fi
