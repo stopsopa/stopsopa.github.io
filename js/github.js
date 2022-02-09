fa68812f909d966a8d9ffe7f9e3e36b60c7328be
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2022-02-09 17:29:55 +0000

    window.do_sort

--- js/github.js
+++ js/github.js
@@ -588,6 +588,52 @@ body .github-profile:hover {
     log.blue('executed NOFOOT', 'handling nofoot attr')
 }());
 
+
+
+// sorting lists [data-do-sort] attribute
+(function () {
+
+  window.do_sort = function () {
+    Array.prototype.slice.call(document.querySelectorAll('[data-do-sort]')).forEach(function (parent) {
+
+      // parent.removeAttribute('data-do-sort');
+
+      var children = manipulation.children(parent)
+
+      var tmp = [];
+
+      children.forEach(function (child) {
+
+        var text = trim(child.tagName ? child.innerText.toLocaleLowerCase() : String(child.textContent));
+
+        text && tmp.push({
+          tagName: child.tagName,
+          node: child,
+          text: text,
+        })
+      });
+
+      var newList = tmp.sort(function (a, b) {
+
+        if (a.text === b.text) {
+
+          return 0
+        }
+
+        return a.text < b.text ? 1 : -1;
+      })
+        .map(function (n) {
+
+          manipulation.prepend(parent, n.node)
+        });
+    });
+
+    log.blue('DOMContentLoaded', 'handling [data-do-sort]', '[defined & triggered in github.js]')
+  }
+
+  document.addEventListener('DOMContentLoaded', window.do_sort);
+}());
+
 // Table of Contents
 // WARNING: it has to be executed in domcontentloaded.js after permalink-my.js
 // WARNING: it has to be executed in domcontentloaded.js after permalink-my.js
@@ -732,46 +778,11 @@ body .github-profile:hover {
                         manipulation.prepend(body, toc);
                     }
 
-// sorting lists [data-do-sort] attribute
-                    (function () {
-                      Array.prototype.slice.call(document.querySelectorAll('[data-do-sort]')).forEach(function (parent) {
-
-                        var children = manipulation.children(parent)
-
-                        var tmp = [];
-
-                        children.forEach(function (child) {
-
-                          var text = trim(child.tagName ? child.innerText.toLocaleLowerCase() : String(child.textContent));
-
-                          text && tmp.push({
-                            tagName: child.tagName,
-                            node: child,
-                            text: text,
-                          })
-                        });
-
-                        var newList = tmp.sort(function (a, b) {
-
-                          if (a.text === b.text) {
-
-                            return 0
-                          }
-
-                          return a.text < b.text ? 1 : -1;
-                        })
-                          .map(function (n) {
-
-                            manipulation.prepend(parent, n.node)
-                          });
-                      });
-
-                      log.blue('DOMContentLoaded', 'handling [data-do-sort]', '[defined & triggered in github.js]')
-                    }());
-
                     // header.innerHTML = `footer`;
 
                     log.blue('DOMContentLoaded TOC', '[toc] found', '[triggered in domcontentloaded.js, delayed async due to DOMContentLoaded and window.async.permalink.then]')
+
+                  window.do_sort();
                 }
             }
 
