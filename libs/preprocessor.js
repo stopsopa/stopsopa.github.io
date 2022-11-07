34b8b61ee9bb6f2dfd28a9556ca05ffee525dcc1
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2022-11-07 21:52:07 +0000

    preprocessorf fix & .vscode settings

--- libs/preprocessor.js
+++ libs/preprocessor.js
@@ -1,31 +1,31 @@
-/**
- * Example of definig EXPOSE_EXTRA_ENV_VARIABLES in .env:
- * 
-    # WARNING: remember to use \ just before $ sign
-    EXPOSE_EXTRA_ENV_VARIABLES="/(^PUBLIC_|^FIREBASE_|^(PROJECT_NAME|NODE_PORT|LOCAL_HOSTS|PROD_SCHEMA|PROD_HOST|GITSTORAGE_CORE_REPOSITORY)\$)/"
- */
-
 const path = require("path");
 
 const fs = require("fs");
 
 const mkdirp = require("mkdirp");
 
+const node = require("detect-node");
+
 const stringToRegex = require("./stringToRegex");
 
 const env = require("./env");
 
-const targets = [
-  path.resolve(__dirname, "..", "preprocessed.js"),
-  // path.resolve(__dirname, "..", "build", "preprocessed.js"),
-];
+const th = (msg) => new Error(`preprocessor.js error: ${msg}`);
 
+const targets = [path.resolve(__dirname, "..", "public", "preprocessed.js"), path.resolve(__dirname, "..", "build", "preprocessed.js")];
+
+if (!node) {
+  throw th(`${__dirname} script should not be ever included in browser code`);
+}
 const result = require("dotenv").config();
 
 // example of variable:
 // EXPOSE_EXTRA_ENV_VARIABLES="/(^PUBLIC_|^REACT_APP_|^(PROJECT_NAME|PORT)$)/"
 const EXPOSE_EXTRA_ENV_VARIABLES = (function (e) {
-  return stringToRegex(e);
+  if (e.includes("$")) {
+    throw new Error(`EXPOSE_EXTRA_ENV_VARIABLES env var should not containe '$' character, replace it with <dollar> instead`);
+  }
+  return stringToRegex(e.replace(/<dollar>/g, "$"));
 })(env("EXPOSE_EXTRA_ENV_VARIABLES"));
 
 const preprocessed = Object.keys(result.parsed).reduce((acc, key) => {
@@ -74,49 +74,49 @@ targets.forEach((target) => {
   fs.writeFileSync(
     target,
     `window.log = (function () {
-  try {
-    return console.log;
-  } catch (e) {
-    return function () {};
-  }
-})();
-
-window.env = (function (e) {
-  return function (key, def) {
-    if (typeof key === "undefined") {
-      return Object.assign({}, e);
-    }
-
-    if (typeof key !== "string") {
-      throw new Error(
-        "preprocessed.js window.env() error: key is not a string"
-      );
-    }
-
-    if (!key.trim()) {
-      throw new Error(
-        "preprocessed.js window.env() error: key is an empty string"
-      );
-    }
-
-    var val = e[key];
-
-    if (typeof val === "undefined") {
-      return def;
-    }
-
-    return val;
-  };
-})(${serialiseInPrettierCompatibileWay(preprocessed)});
-
-log("const env = window.env");
-`
+      try {
+        return console.log;
+      } catch (e) {
+        return function () {};
+      }
+    })();
+    
+    window.env = (function (e) {
+      return function (key, def) {
+        if (typeof key === "undefined") {
+          return Object.assign({}, e);
+        }
+    
+        if (typeof key !== "string") {
+          throw new Error(
+            "preprocessed.js window.env() error: key is not a string"
+          );
+        }
+    
+        if (!key.trim()) {
+          throw new Error(
+            "preprocessed.js window.env() error: key is an empty string"
+          );
+        }
+    
+        var val = e[key];
+    
+        if (typeof val === "undefined") {
+          return def;
+        }
+    
+        return val;
+      };
+    })(${serialiseInPrettierCompatibleWay(preprocessed)});
+    
+    log("const env = window.env");
+    `
   );
 
   if (!fs.existsSync(target)) {
     // warning: if under dirfile there will be broken link (pointing to something nonexisting) then this function will return false even if link DO EXIST but it's broken
 
-    throw new Error(`File '${target}' creation failed`);
+    throw th(`File '${target}' creation failed`);
   }
 });
 
@@ -140,7 +140,7 @@ function serialiseString(key, always) {
   }
 }
 
-function serialiseInPrettierCompatibileWay(preprocessed) {
+function serialiseInPrettierCompatibleWay(preprocessed) {
   let tmp = [];
   Object.entries(preprocessed).forEach(([key, value]) => {
     tmp.push(`  ${serialiseString(key)}: ${serialiseString(value, true)}`);
@@ -148,8 +148,8 @@ function serialiseInPrettierCompatibileWay(preprocessed) {
 
   if (tmp.length > 0) {
     return `{
-${tmp.join(",\n")},
-}`;
+    ${tmp.join(",\n")},
+    }`;
   } else {
     return `{}`;
   }
