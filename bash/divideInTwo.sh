946786b918d8ab3d7f3905440c707c6756499f2a
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2022-12-30 18:19:30 +0000

    among us

--- bash/divideInTwo.sh
+++ bash/divideInTwo.sh
@@ -12,3 +12,14 @@ function divideInTwo {
 
   printf "${1:0:${#1}/2}${DELIMITER}${1:${#1}/2}"
 }
+
+#function divideInTwo {
+#
+#  DELIMITER="\${2}"
+#
+#  if [ "\${DELIMITER}" = "" ]; then
+#    DELIMITER="█▓▒░■░▒▓█"
+#  fi
+#
+#  printf "░▒▓█\${1:0:\${#1}/2}\${DELIMITER}\${1:\${#1}/2}█▓▒░"
+#}
