03c24e470bbf9b3e26332129fc9e75bdbdb64199
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2023-02-26 01:16:47 +0000

    ace

--- pages/macro/Ace.jsx
+++ pages/macro/Ace.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 
 import waitForAce from "./lib/waitForAce";
 
@@ -6,32 +6,32 @@ import RecordLog from "./RecordLog";
 
 // import "./Ace.css";
 
-// let i = 0;
-
 export default ({ id, content, onChange, recordOn }) => {
-  const refId = useRef(
-    (function (id) {
-      let resolve;
-      const promise = new Promise((res) => (resolve = res));
-      return {
-        id,
-        log: function (...args) {
-          try {
-            console.log(id, ...args);
-          } catch (e) {
-            console.log("log error: ", e);
-          }
-        },
-        editor: undefined,
-        promise,
-        resolve,
-      };
-    })("react-ace-" + id)
-  );
+  const refIdMemo = useMemo(() => {
+    const id_ = "react-ace-" + id;
+
+    let resolve;
+
+    const promise = new Promise((res) => (resolve = res));
+
+    return {
+      id: id_,
+      log: function (...args) {
+        try {
+          console.log(id_, ...args);
+        } catch (e) {
+          console.log("log error: ", e);
+        }
+      },
+      editor: undefined,
+      promise,
+      resolve,
+    };
+  }, []);
+
+  const refId = useRef(refIdMemo);
 
   const log = refId.current.log;
-
-  // window.debug || log(`Ace render--------------${id}------------${content}-----<<<<`);
 
   const divRef = useRef(null);
 
@@ -42,94 +42,147 @@ export default ({ id, content, onChange, recordOn }) => {
   });
 
   useEffect(() => {
-    const div = divRef.current;
+    refId.current.content = content;
+    refId.current.promise.then((editor) => {
+      if (typeof refId.current.content !== "string") {
+        return;
+      }
 
-    div.innerText = "Loading...";
-    divRef.current.setAttribute("data-record", "0");
+      if (!refId.current.update) {
+        return log("skip update flag true");
+      }
 
+      const edContent = editor.getValue();
+      if (edContent !== refId.current.content) {
+        editor.setValue(refId.current.content + "", -1);
+
+        delete refId.current.content;
+      }
+    });
+  }, [refId, content]);
+
+  useEffect(() => {
     (async function () {
-      // window.debug || log(`---seq []`);
-      /**
-       * I just need to wait for ace here, because it will be loaded by github.js
-       */
-      await waitForAce();
+      const div = divRef.current;
 
-      refId.current.editor = mountEditor(div, {
-        log,
-        refId,
-        divRef,
-        content,
-        onChange,
-        id,
-      });
+      div.innerText = "Loading...";
+
+      divRef.current.setAttribute("data-record", "0");
+
+      await waitForAce();
 
       if (!window.editors) {
         window.editors = {};
       }
 
+      divRef.current.parentNode.classList.add("mounting");
+
+      const editor = ace.edit(div);
+
+      refId.current.resolve(editor);
+
+      const session = editor.getSession();
+
+      session.setOptions({ tabSize: 4, useSoftTabs: true, maxLines: Infinity });
+
+      editor.setTheme("ace/theme/idle_fingers");
+
+      session.setUseWrapMode(true);
+
+      editor.setShowInvisibles(true);
+
+      let lang = "javascript";
+      session.setMode("ace/mode/" + lang);
+
+      if (typeof content === "string") {
+        editor.setValue(content);
+      }
+
+      editor.clearSelection();
+
+      let heightUpdateFunction = function () {
+        // log("heightUpdateFunction");
+
+        // http://stackoverflow.com/questions/11584061/
+        let newHeight = session.getScreenLength() * editor.renderer.lineHeight + editor.renderer.scrollBar.getWidth();
+
+        let height = newHeight.toString();
+
+        // 46 rows in ace editor in 735px of window.innerHeight:
+        // so: 735 / 46 = 15.97 - pixels per row
+        // header takes 6 ace editor rows height so we will have to subctract always this
+        //   6 * 15.97 = 95.82px
+        const pixelsPerAceEditorLine = 15.97;
+
+        const reserveLinesForHeaders = 10;
+
+        const reservedHeightPixelsForHeaders = reserveLinesForHeaders * pixelsPerAceEditorLine;
+
+        let minHeight = parseInt(window.innerHeight - reservedHeightPixelsForHeaders, 10);
+
+        minHeight += "px";
+
+        height += "px";
+
+        div.style.height = minHeight > height ? minHeight : height;
+
+        // log("div.style.height", div.style.height, "minHeight: ", minHeight, "height: ", height, "minHeight > height", minHeight > height);
+
+        editor.resize();
+      };
+
+      heightUpdateFunction();
+
+      refId.current.heightUpdateFunction = heightUpdateFunction;
+
+      divRef.current.parentNode.classList.remove("mounting");
+
+      session.on("change", () => {
+        if (typeof onChange === "function") {
+          onChange(session.getValue());
+        }
+      });
+
+      refId.current.update = true;
+
+      editor.on("focus", function () {
+        log("focus");
+        refId.current.update = false;
+        RecordLog.focusedEditor(editor);
+      });
+
+      editor.on("blur", function () {
+        log("blur");
+        refId.current.update = true;
+      });
+
       window.editors[id] = onChange;
 
-      // this is here for testing mount and unmount
-      //   let mount = false;
-      //   let i = 0;
-      //   document.body.addEventListener("click", () => {
-      //     mount = !mount;
-      //     if (mount) {
-      //       if (refId.current.editor) {
-      //         console.log("body click: editor exist: editor.destroy()");
-      //         refId.current.editor.destroy();
-      //         refId.current.editor = undefined;
-      //         div.removeAttribute("class");
-      //         div.removeAttribute("style");
-      //       } else {
-      //         console.log("body click: editor doesn't exist: editor.destroy()");
-      //       }
-      //     } else {
-      //       i += 1;
-      //       console.log("body click: mount editor");
-      //       refId.current.editor = mountEditor(div, {
-      //         content: `
-      //         if (attr[i].name.toLowerCase() === "class") {
-      //             continue;
-      //         }    second ${i}
-      //         `,
-      //       });
-      //     }
-      //   });
+      refId.current.editor = editor;
+
+      window.addEventListener("resize", refId.current.heightUpdateFunction);
+
+      document.addEventListener("scroll", refId.current.heightUpdateFunction);
     })();
 
     return () => {
       if (refId.current.editor) {
-        // console.log("useFetch unmount: editor exist: editor.destroy()");
         refId.current.editor.destroy();
+
         refId.current.editor = undefined;
-        div.removeAttribute("class");
-        div.removeAttribute("style");
       } else {
         console.log("useFetch unmount: editor doesn't exist: editor.destroy()");
       }
+
+      div.removeAttribute("class");
+
+      div.removeAttribute("style");
+
+      window.removeEventListener("resize", refId.current.heightUpdateFunction);
+
+      document.removeEventListener("scroll", refId.current.heightUpdateFunction);
     };
   }, []);
-
-  useEffect(() => {
-    // i += 1;
-    // window.debug || log(`useEffect refId content>${content}<`);
-    refId.current.promise.then((editor) => {
-      if (!refId.current.update) {
-        return log("skip update flag true");
-      }
-      const edContent = editor.getValue();
-      if (edContent !== content) {
-        // log(`useEffect update ace>${edContent}< ref>${content}<`);
-
-        // if (i > 10) {
-        //   return log("stop...");
-        // }
-        editor.setValue(content + "", -1);
-        // refId.current.content = content
-      }
-    });
-  }, [refId, content]);
 
   function onAfterExec(e) {
     // RecordLog.add(refId.current.editor.getCursorPosition(), e);
@@ -137,23 +190,15 @@ export default ({ id, content, onChange, recordOn }) => {
   }
 
   function onFindType(e) {
-    var el = e.target;
+    let el = e.target;
 
-    var match = el.matches(".ace_search_field");
+    let match = el.matches(".ace_search_field");
 
     if (match) {
       try {
         RecordLog.add({
           command: { name: "_delegation_typefind" },
           args: (function (a) {
-            // ed().execCommand("find", {
-            //   needle: "zz",
-            //   wrap: true,
-            //   caseSensitive: false,
-            //   wholeWord: false,
-            //   regExp: false,
-            // });
-
             return {
               needle: el.value,
               wrap: a.wrap,
@@ -171,9 +216,10 @@ export default ({ id, content, onChange, recordOn }) => {
 
   function turnRecordOn() {
     RecordLog.reset();
+
     refId.current.editor.commands.on("afterExec", onAfterExec);
 
-    Array.from(document.querySelectorAll(".ace_editor")).forEach((el) => {
+    Array.from(divRef.current.querySelectorAll(".ace_editor")).forEach((el) => {
       el.addEventListener("input", onFindType);
     });
 
@@ -183,7 +229,7 @@ export default ({ id, content, onChange, recordOn }) => {
   function turnRecordOff() {
     refId.current.editor.commands.off("afterExec", onAfterExec);
 
-    Array.from(document.querySelectorAll(".ace_editor")).forEach((el) => {
+    Array.from(divRef.current.querySelectorAll(".ace_editor")).forEach((el) => {
       el.removeEventListener("input", onFindType);
     });
 
@@ -191,8 +237,6 @@ export default ({ id, content, onChange, recordOn }) => {
   }
 
   useEffect(() => {
-    // window.debug || log(`---seq [recordOn]`, recordOn);
-
     if (typeof recordOn === "boolean" && refId.current.editor) {
       if (recordOn === true) {
         turnRecordOn();
@@ -209,98 +253,3 @@ export default ({ id, content, onChange, recordOn }) => {
 
   return <div ref={divRef} id={id} />;
 };
-
-function mountEditor(div, { log, divRef, refId, content, onChange, id }) {
-  refId.current.update = true;
-
-  divRef.current.parentNode.classList.add("mounting");
-
-  const editor = ace.edit(div);
-
-  refId.current.resolve(editor);
-
-  const session = editor.getSession();
-
-  //   editors[un] = editor;
-
-  session.setOptions({ tabSize: 4, useSoftTabs: true });
-  // session.setTabSize(4);
-  editor.setTheme("ace/theme/idle_fingers");
-  session.setUseWrapMode(true);
-
-  editor.setShowInvisibles(true);
-
-  let lang = "javascript";
-  session.setMode("ace/mode/" + lang);
-  //   editor.setValue(_.unescape(t).replace(/^ *\n([\s\S]*?)\n *$/g, "$1"));
-
-  if (refId.current.update) {
-    // window.debug || log("update on", content);
-    if (typeof content === "string") {
-      editor.setValue(content);
-    }
-  } else {
-    // window.debug || log("update off");
-  }
-  editor.clearSelection();
-
-  var heightUpdateFunction = function () {
-    // http://stackoverflow.com/questions/11584061/
-    var newHeight = session.getScreenLength() * editor.renderer.lineHeight + editor.renderer.scrollBar.getWidth();
-
-    // log('new height', newHeight);
-
-    var h = newHeight.toString();
-
-    // h += 1000;
-
-    h += "px";
-
-    div.style.height = h;
-
-    // Array.prototype.slice.call(document.querySelector('.editor').querySelectorAll('[class]'))
-    //     .map(e => e.style.height = h)
-    // ;
-    // $('#editor').height(newHeight.toString() + "px");
-    // $('#editor-section').height(newHeight.toString() + "px");
-
-    // This call is required for the editor to fix all of
-    // its inner structure for adapting to a change in size
-    editor.resize();
-    editor.focus();
-  };
-
-  // Set initial size to match initial content
-  heightUpdateFunction();
-
-  refId.current.heightUpdateFunction = heightUpdateFunction;
-
-  // setTimeout(() => {
-  divRef.current.parentNode.classList.remove("mounting");
-
-  heightUpdateFunction();
-  // }, 50);
-
-  // Whenever a change happens inside the ACE editor, update
-  // the size again
-  session.on("change", () => {
-    heightUpdateFunction();
-    if (typeof onChange === "function") {
-      log(`onChange >${session.getValue()}<`);
-      onChange(session.getValue());
-    }
-  });
-
-  editor.on("focus", function () {
-    // window.debug || log("focus");
-    refId.current.update = false;
-    RecordLog.focusedEditor(editor);
-  });
-
-  editor.on("blur", function () {
-    // window.debug || log("blur");
-    refId.current.update = true;
-  });
-
-  return editor;
-}
