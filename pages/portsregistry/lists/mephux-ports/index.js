d4a3132788070560eee130efbd7431cc81c9a345
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2024-12-06 22:46:08 +0000

    jj

--- pages/portsregistry/lists/mephux-ports/index.js
+++ pages/portsregistry/lists/mephux-ports/index.js
@@ -2,7 +2,13 @@
  * from: https://github.com/mephux/ports.json/blob/master/ports.lists.json
  */
 
-import list from "./ports.lists.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const list = JSON.parse(fs.readFileSync(path.resolve(__dirname, "ports.lists.json"), "utf-8"));
 
 import uniq from "lodash/uniq.js";
 
--- pages/portsregistry/lists/stoutZero_commonly-used-ports/index.js
+++ pages/portsregistry/lists/stoutZero_commonly-used-ports/index.js
@@ -2,7 +2,13 @@
  * from: https://gist.github.com/stoutZero/15d334dea71a604cade777dee2f7a9e5
  */
 
-import list from "./commonly-used-ports.json" assert { type: "json" };
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const list = JSON.parse(fs.readFileSync(path.resolve(__dirname, "commonly-used-ports.json"), "utf-8"));
 
 import uniq from "lodash/uniq.js";
 
