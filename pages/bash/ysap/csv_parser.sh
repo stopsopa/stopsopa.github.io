d8d8cd83248c4266c8cf533f4861c2d6e1acd255
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2025-12-27 22:14:11 +0000

    tweaks q

--- pages/bash/ysap/csv_parser.sh
+++ pages/bash/ysap/csv_parser.sh
@@ -1,10 +1,36 @@
 
-if [ -n "${ZSH_VERSION:-}" ]; then
-  _SHELL=zsh
-elif [ -n "${BASH_VERSION:-}" ]; then
-  _SHELL=bash
-else
-  _SHELL=sh
-fi
+_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
+set -x
 
-echo ">$_SHELL<"
+# https://youtu.be/Sx9zG7wa4FA?t=11756
+cat "${_DIR}/people.csv" | cut -d , -f 3
+# city
+# New York
+# Chicago
+# Boston
+# San Francisco
+
+cat "${_DIR}/people.csv" | cut -d , -f 1,3
+# name, city
+# Alice, New York
+# Bob, Chicago
+# Charlie, Boston
+# Dana, San Francisco
+
+cat "${_DIR}/people.csv" | cut -d , -f 1-3 | tr , '\t'
+# name     age     city
+# Alice    30      New York
+# Bob      25      Chicago
+# Charlie  35      Boston
+# Dana     28      San Francisco
+
+cat "${_DIR}/complex.csv" | cut -d , -f 1-3 | tr , '\t'
+# that won't work unfortunately, this is just comma separated values, not proper csv parsers
+# name     age     city
+# Alice    30      "New York
+# Bob      25      Chicago
+# Charlie  35      Boston
+# Dana     28      San Francisco
+
+
+
