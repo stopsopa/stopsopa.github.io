366a9bbca24c53bbc232a0ce8b400aa5ef88b652
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-04-19 17:59:04 +0100

    d

--- .env.sh
+++ .env.sh
@@ -8,56 +8,16 @@ if [[ -f "${DIR}/${ENVFILE}" ]]; then
     eval "$(/bin/bash "${DIR}/bash/exportsource.sh" "${DIR}/${ENVFILE}")"
 fi
 
+source "${DIR}/bash/require_non_empty_var.sh"
+
 # -----------------------------------------------------------------------------
 # Validation
 # -----------------------------------------------------------------------------
 
-re_number='^[0-9]+$'
-
-# Validate NODE_API_PORT
-if [[ -z "${NODE_API_PORT}" ]]; then
-    echo "${0} error: NODE_API_PORT is not set"
-    exit 1
-fi
-if ! [[ "${NODE_API_PORT}" =~ ${re_number} ]]; then
-   echo "${0} error: NODE_API_PORT=>${NODE_API_PORT}< is not a number"
-   exit 1
-fi
-
-if [[ -z "${NODE_API_PORT_HTTPS}" ]]; then
-    echo "${0} error: NODE_API_PORT_HTTPS is not set"
-    exit 1
-fi
-if ! [[ "${NODE_API_PORT_HTTPS}" =~ ${re_number} ]]; then
-   echo "${0} error: NODE_API_PORT_HTTPS=>${NODE_API_PORT_HTTPS}< is not a number"
-   exit 1
-fi
-
-if [[ -z "${LOCAL_HOSTS}" ]]; then
-    echo "${0} error: LOCAL_HOSTS is not set"
-    exit 1
-fi
-
-if [[ -z "${FLAG}" ]]; then
-    echo "${0} error: FLAG is not set"
-    exit 1
-fi
-
-if [[ -z "${PROD_SCHEMA}" ]]; then
-    echo "${0} error: PROD_SCHEMA is not set"
-    exit 1
-fi
-
-if [[ -z "${PROD_HOSTNAME}" ]]; then
-    echo "${0} error: PROD_HOSTNAME is not set"
-    exit 1
-fi
-
-if [[ -z "${PROD_PORT}" ]]; then
-    echo "${0} error: PROD_PORT is not set"
-    exit 1
-fi
-if ! [[ "${PROD_PORT}" =~ ${re_number} ]]; then
-   echo "${0} error: PROD_PORT=>${PROD_PORT}< is not a number"
-   exit 1
-fi
+require_non_empty_and_matching_var "${0}" "NODE_API_PORT" "^[0-9]+$"
+require_non_empty_and_matching_var "${0}" "NODE_API_PORT_HTTPS" "^[0-9]+$"
+require_non_empty_var "${0}" "LOCAL_HOSTS"
+require_non_empty_var "${0}" "FLAG"
+require_non_empty_var "${0}" "PROD_SCHEMA"
+require_non_empty_var "${0}" "PROD_HOSTNAME"
+require_non_empty_and_matching_var "${0}" "PROD_PORT" "^[0-9]+$"
--- bash/require_non_empty_var.sh
+++ bash/require_non_empty_var.sh
@@ -1,4 +1,31 @@
 
+# 
+# Usage in .env.sh
+
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# ENVFILE="${ENVFILE:-.env}"
+
+# if [[ -f "${DIR}/${ENVFILE}" ]]; then
+#     # https://unix.stackexchange.com/a/79065
+#     eval "$(/bin/bash "${DIR}/bash/exportsource.sh" "${DIR}/${ENVFILE}")"
+# fi
+
+# source "${DIR}/bash/require_non_empty_var.sh"
+
+# # -----------------------------------------------------------------------------
+# # Validation
+# # -----------------------------------------------------------------------------
+
+# require_non_empty_and_matching_var "${0}" "NODE_API_PORT" "^[0-9]+$"
+# require_non_empty_and_matching_var "${0}" "NODE_API_PORT_HTTPS" "^[0-9]+$"
+# require_non_empty_var "${0}" "LOCAL_HOSTS"
+# require_non_empty_var "${0}" "FLAG"
+# require_non_empty_var "${0}" "PROD_SCHEMA"
+# require_non_empty_var "${0}" "PROD_HOSTNAME"
+# require_non_empty_and_matching_var "${0}" "PROD_PORT" "^[0-9]+$"
+#  
+
 
 
 # simple reusable functon to check if env var is defined and not empty
