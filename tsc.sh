ed90af2da790c658a1a7d26804a66eb5e1c34fe6
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-05-09 16:49:57 +0100

    g

--- tsc.sh
+++ tsc.sh
@@ -1,16 +1,21 @@
 
 
 # to find all ts files
-find . \( \
-        -type d -name node_modules -prune -o \
-        -type d -name .git -prune -o \
-        -type d -name coverage -prune -o \
-        -type d -name noprettier -prune \
-    \) \
-    -o \
-    \( -type f \( -name "*.ts" \) -print \)
+# find . \( \
+#         -type d -name node_modules -prune -o \
+#         -type d -name .git -prune -o \
+#         -type d -name coverage -prune -o \
+#         -type d -name noprettier -prune \
+#     \) \
+#     -o \
+#     \( -type f \( -name "*.ts" \) -print \)
 
+TSCONFIG=tsc.ignore.tsconfig.json
 
-NODE_OPTIONS= node tsc.ignore.ts "tsc.ignore" tsconfig.json tsc.ignore.tsconfig.json
+NODE_OPTIONS= node tsc.ignore.ts "tsc.ignore" tsconfig.json "${TSCONFIG}"
 
-npx tsc -p tsconfig.json
+# typechecking more file
+npx tsc -p tsconfig.json --noEmit
+
+# # but transpiling only subset of files
+# npx tsc -p ${TSCONFIG} "$@"
