3a835345f372a769719ec0a4acbdce2810450c03
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-05-09 18:42:14 +0100

    d

--- transpile.sh
+++ transpile.sh
@@ -4,12 +4,13 @@
 # /bin/bash transpile.sh --watch
 # 
 
-find . \( \
-        -type d -name node_modules -prune -o \
-        -type d -name .git -prune -o \
-        -type d -name coverage -prune -o \
-        -type d -name noprettier -prune \
-    \) \
-    -o \
-    \( -type f \( -name "*.ts" \) -print \) \
+find . -type d \( \
+        -name node_modules \
+        -o -name .git \
+        -o -name coverage \
+        -o -name noprettier \
+    \) -prune \
+    -o -type \
+    f -name "*.ts" \
+    -print \
     | NODE_OPTIONS="" npx tsx transpile.ts "$@"
