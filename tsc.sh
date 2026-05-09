d17b3778dfcd1692498e25229df6cb5b1da927bb
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-05-09 17:01:53 +0100

    typechecking

--- tsc.sh
+++ tsc.sh
@@ -1,4 +1,14 @@
-
+# 
+# I had hope to use this script for typechecking and in another step add some excluding rules to 
+# tsconfig.json and this way generating new typescript config tsc.ignore.tsconfig.json
+# and with it use tsc for transpilation but eventually I've discovered that it as usual
+# complains with:
+#   Option 'allowImportingTsExtensions' can only be used when either 'noEmit' or 'emitDeclarationOnly' is set.ts
+# when I remove "noEmit": true, in tsconfig.json
+# so I will use esbuild.ts for transpilation then
+# 
+# So in conslusion, we will only use this for typechecking
+# 
 
 # to find all ts files
 # find . \( \
@@ -12,7 +22,7 @@
 
 TSCONFIG=tsc.ignore.tsconfig.json
 
-NODE_OPTIONS= node tsc.ignore.ts "tsc.ignore" tsconfig.json "${TSCONFIG}"
+#NODE_OPTIONS= node tsc.ignore.ts "tsc.ignore" tsconfig.json "${TSCONFIG}"
 
 # typechecking more file
 npx tsc -p tsconfig.json --noEmit
