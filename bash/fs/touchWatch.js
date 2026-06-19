8f71f624d011c9eb84202006ace83a803ba2b225
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-06-19 14:28:01 +0100

    q

--- bash/fs/touchWatch.js
+++ bash/fs/touchWatch.js
@@ -1,53 +1,41 @@
-/**
- * Fluentbit seems to have problems with filesystem watch capabilities if its executed in docker container
- * because it have access to server output log file via volumes.
- * For some reason mounting directories from host machine to docker container does not
- * trigger watch event for processess watching files from inside container
- *
- * Therefore we are using this script to watch file on the host machine and "touching" it
- * it seems to solve the problem for process inside docker container.
- *
- * Process inside container (In this case fluentbit) is able to immediately detect change on server log file
- */
-const fs = require("fs");
-
-const fsp = require("node:fs/promises");
-
-const file = process.argv[2];
-
-const th = (msg) => new Error(`watch.cjs error: ${msg}`);
-
-if (typeof file !== "string") {
-  throw th(`file to watch not specified`);
-}
-
-if (!fs.lstatSync(file).isFile()) {
-  throw th(`target '${file}' is not a file`);
-}
-
-let doTouch = true;
-
-fs.watchFile(
-  file,
-  {
-    interval: 50,
-  },
-  async (curr, prev) => {
-    if (doTouch) {
-      doTouch = false;
-
-      const time = new Date();
-
-      await fsp.utimes(file, time, time).catch(async function (err) {
-        // https://remarkablemark.org/blog/2017/12/17/touch-file-nodejs/#touch-file
-        if ("ENOENT" !== err.code) {
-          throw err;
-        }
-      });
-
-      setTimeout(() => {
-        doTouch = true;
-      }, 80);
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __commonJS = (cb, mod) =>
+  function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+import fs from "node:fs";
+import * as fsp from "node:fs/promises";
+var require_touchWatch = __commonJS({
+  "bash/fs/touchWatch.ts"() {
+    const file = process.argv[2];
+    const th = (msg) => new Error(`watch.ts error: ${msg}`);
+    if (typeof file !== "string") {
+      throw th("file to watch not specified");
     }
-  }
-);
+    if (!fs.lstatSync(file).isFile()) {
+      throw th(`target '${file}' is not a file`);
+    }
+    let doTouch = true;
+    fs.watchFile(
+      file,
+      {
+        interval: 50,
+      },
+      async (_curr, _prev) => {
+        if (doTouch) {
+          doTouch = false;
+          const time = /* @__PURE__ */ new Date();
+          await fsp.utimes(file, time, time).catch(async (err) => {
+            if (err.code !== "ENOENT") {
+              throw err;
+            }
+          });
+          setTimeout(() => {
+            doTouch = true;
+          }, 80);
+        }
+      }
+    );
+  },
+});
+export default require_touchWatch();
--- bash/node/is-port-free.js
+++ bash/node/is-port-free.js
@@ -1,86 +1,69 @@
+#!/usr/bin/env node
+import http from "node:http";
+const filename = import.meta.url;
 if (process.argv[2] === "--help" || process.argv.length < 3) {
   console.log(`
 
 Script checking if port is free and can be allocated, usage:
-  
+
   Mode 1:
 
-    node ${__filename} localhost:80   
-    node ${__filename} 0.0.0.0:8080 
-    
+    node script.mjs localhost:80
+    node script.mjs 0.0.0.0:8080
+
     script echos to console 1 if port is free or 0 if port can't be taken (no access or already used)
-    
+
     exit code in both cases will be 0
-    
-    if any other error appear then it will show as unhandled error and thus will cause exit code to be non zero  
-  
+
+    if any other error appear then it will show as unhandled error and thus will cause exit code to be non zero
+
   Mode 2:
 
-    node ${__filename} localhost:80 -v
-    node ${__filename} 0.0.0.0:8080 -v
-    
-    script echos human readable message whether port is free or not
-       
-    if port is free exit code will be 0
-    
-    if it is not free exit code will be non 0      
-  
-`);
+    node script.mjs localhost:80 -v
+    node script.mjs 0.0.0.0:8080 -v
 
+    script echos human readable message whether port is free or not
+
+    if port is free exit code will be 0
+
+    if it is not free exit code will be non 0
+
+`);
   process.exit(1);
 }
-
-if (typeof process.argv[2] !== "string") {
+const arg = process.argv[2];
+if (typeof arg !== "string") {
   throw new Error(`process.argv[2] !== 'string'`);
 }
-
-const reg = /^(localhost|\d{1,2}\.\d{1,2}\.\d{1,2}\.\d{1,2}):(\d+)$/;
-
-const match = process.argv[2].match(reg);
-
+const reg = /^(localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(\d+)$/;
+const match = arg.match(reg);
 if (match === null) {
-  throw new Error(`process.argv[2] (${process.argv[2]}) don't match: ${reg}`);
+  throw new Error(`process.argv[2] (${arg}) don't match: ${reg}`);
 }
-
 const host = match[1];
-
-const port = parseInt(match[2], 10);
-
-const v = process.argv.includes("--verbose") || process.argv.includes("-v");
-
-const http = require("http");
-
+const port = Number.parseInt(match[2], 10);
+const verbose = process.argv.includes("--verbose") || process.argv.includes("-v");
 const app = http.createServer(() => {});
-
 app
   .listen(port, host, () => {
-    if (v) {
-      console.log(`node ${__filename}: port ${host}:${port} is free`);
+    if (verbose) {
+      console.log(`node ${filename}: port ${host}:${port} is free`);
     } else {
       process.stdout.write("1");
     }
-
     process.exit(0);
   })
   .on("error", (e) => {
-    const s = String(e);
-
-    // if (s.includes(' EADDRINUSE ') || s.includes(' EACCES ')) {
     if (e.code === "EADDRINUSE" || e.code === "EACCES") {
-      // https://nodejs.org/api/net.html#net_server_listen
-
-      if (v) {
-        console.log(`node ${__filename} error: port ${host}:${port} is not free: ${s}`);
-
+      if (verbose) {
+        console.log(`node ${filename} error: port ${host}:${port} is not free: ${String(e)}`);
         process.exit(1);
       } else {
         process.stdout.write("0");
-
         process.exit(0);
       }
     } else {
-      console.log(`node ${__filename} error: port ${host}:${port} is not free due to unknown reason:`);
-
+      console.log(`node ${filename} error: port ${host}:${port} is not free due to unknown reason:`);
       throw e;
     }
   });
