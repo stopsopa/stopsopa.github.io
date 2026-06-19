4a1e98dd6fb0478d7c9b47b597a45777a86ed9c6
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-06-19 14:31:47 +0100

    q

--- pages/bash/ssh_sync_watch/sync_from_ide_auto.sh
+++ pages/bash/ssh_sync_watch/sync_from_ide_auto.sh
@@ -8,6 +8,7 @@ SSH_HOST="ssh-domain.com"
 SSH_PORT="22"
 REMOTE_DIR="/home/somwhereremote"
 SOCKET_PATH="${HOME}/.ssh/roles_sock"
+SSH_PASS="xxxx"
 # ---------------------
 
 S="\\"
@@ -19,6 +20,22 @@ if ! command -v fswatch >/dev/null 2>&1; then
   echo "👉 brew install fswatch"
   exit 1
 fi
+
+if ! command -v sshpass >/dev/null 2>&1; then
+  echo "❌ sshpass missing"
+  echo ""
+  echo "👉 Install:"
+  echo "   macOS: brew install hudochenkov/sshpass/sshpass"
+  echo "   Linux: sudo apt install sshpass"
+  exit 1
+fi
+
+if [[ -z "${SSH_PASS}" ]]; then
+  echo "❌ SSH_PASS is not set"
+  echo "👉 export SSH_PASS='your_password'"
+  exit 1
+fi
+# ---------------------------
 
 # start SSH multiplexing
 ssh -M -S "${SOCKET_PATH}" -fnNT \
@@ -37,14 +54,26 @@ fswatch -0 ./ | while read -d "" file; do
   REMOTE_PATH="${REMOTE_DIR}/${rel_path}"
 
   if [[ -e "${file}" ]]; then
+    # prevent race-condition rsync on disappearing files
+    [[ -f "${file}" || -d "${file}" ]] || continue
+
     echo "✅ File exists: ${file}"
     # --- FILE EXISTS: Sync it ---
-    CMD="rsync -az -e \"ssh -p ${SSH_PORT} -S ${SOCKET_PATH}\" \"${file}\" \"${SSH_USER}@${SSH_HOST}:$REMOTE_PATH\""
+
+    CMD="sshpass -p \"${SSH_PASS}\" rsync -az \
+      -e \"ssh -p ${SSH_PORT} -S ${SOCKET_PATH}\" \
+      \"${file}\" \
+      \"${SSH_USER}@${SSH_HOST}:${REMOTE_PATH}\""
+
   else
     echo "❌ File deleted: ${file}"
     # --- FILE DELETED LOCALLY: Remove remotely if it's a file ---
     # [ -f ... ] checks if it exists and is a regular file, then rm removes it.
-    CMD="ssh -p ${SSH_PORT} -S ${SOCKET_PATH} ${SSH_USER}@${SSH_HOST} '[ -f \"${REMOTE_PATH}\" ] && rm \"${REMOTE_PATH}\"'"
+
+    CMD="sshpass -p \"${SSH_PASS}\" ssh \
+      -p ${SSH_PORT} -S ${SOCKET_PATH} \
+      ${SSH_USER}@${SSH_HOST} \
+      '[ -f \"${REMOTE_PATH}\" ] && rm \"${REMOTE_PATH}\"'"
   fi
 
   printf '%s\n\n' "  ${CMD}"
