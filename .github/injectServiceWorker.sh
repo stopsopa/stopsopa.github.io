363beec56f88a8b06269ec90386add2c69fd595d
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-06-29 00:07:32 +0100

    q

--- .github/injectServiceWorker.sh
+++ .github/injectServiceWorker.sh
@@ -1,3 +1,7 @@
+# 
+# time /bin/bash .github/injectServiceWorker.sh
+# 
+
 grep -Z '<script type="module" src="/js/github.js"></script>' -rl \
   --exclude-dir={node_modules,.git,noprettier,dist,playwright-report,jasmine} \
   --include '*.html' |
