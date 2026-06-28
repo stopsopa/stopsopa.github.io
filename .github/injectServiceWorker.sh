996aa7822937d5577f940fdc8d0be4631dd5c732
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-06-28 23:19:24 +0100

    l

--- .github/injectServiceWorker.sh
+++ .github/injectServiceWorker.sh
@@ -1,11 +1,15 @@
 grep -Z '<script type="module" src="/js/github.js"></script>' -rl \
   --exclude-dir={node_modules,.git,noprettier,dist,playwright-report,jasmine} \
   --include '*.html' |
-xargs -0 sed -i '0,/<head>/{
-/<head>/i\
-<script>\
-  if ("serviceWorker" in navigator) {\
-    navigator.serviceWorker.register("/sw.js");\
-  }\
+while IFS= read -r -d '' file; do
+  perl -0777 -i -pe '
+    s{<head>}{
+<script>
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/sw.js");
+  }
 </script>
-}'
+<head>
+}s if $. == 1
+  ' "$file"
+done
