5614dcbe85d75071468bc588ea692cb2bac67fee
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-07-14 21:54:45 +0100

    levenshtein q

---
+++ pages/node/levenshtein/levenshtein.js
@@ -0,0 +1,24 @@
+function levenshtein(a, b) {
+  const dp = [];
+  for (let i = 0; i <= a.length; i++) {
+    dp[i] = [i];
+  }
+  for (let j = 1; j <= b.length; j++) {
+    dp[0][j] = j;
+  }
+  for (let i = 1; i <= a.length; i++) {
+    for (let j = 1; j <= b.length; j++) {
+      const cost = a[i - 1] === b[j - 1] ? 0 : 1;
+      dp[i][j] = Math.min(
+        dp[i - 1][j] + 1,
+        // deletion
+        dp[i][j - 1] + 1,
+        // insertion
+        dp[i - 1][j - 1] + cost
+        // substitution
+      );
+    }
+  }
+  return dp[a.length][b.length];
+}
+export { levenshtein as default };
---
+++ pages/node/levenshtein/levenshtein.ts
@@ -0,0 +1,25 @@
+export default function levenshtein(a: string, b: string): number {
+  const dp: number[][] = [];
+
+  for (let i = 0; i <= a.length; i++) {
+    dp[i] = [i];
+  }
+
+  for (let j = 1; j <= b.length; j++) {
+    dp[0][j] = j;
+  }
+
+  for (let i = 1; i <= a.length; i++) {
+    for (let j = 1; j <= b.length; j++) {
+      const cost = a[i - 1] === b[j - 1] ? 0 : 1;
+
+      dp[i][j] = Math.min(
+        dp[i - 1][j] + 1, // deletion
+        dp[i][j - 1] + 1, // insertion
+        dp[i - 1][j - 1] + cost // substitution
+      );
+    }
+  }
+
+  return dp[a.length][b.length];
+}
