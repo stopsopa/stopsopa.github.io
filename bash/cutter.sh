8b6d03dc188d7bd109641a9f3b70882ceb586cd2
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-07-25 00:53:17 +0100

    c

--- bash/cutter.sh
+++ bash/cutter.sh
@@ -6,38 +6,63 @@
 #
 # Usage:
 #
-#
+# see https://github.com/stopsopa/stopsopa.github.io/blob/master/bash/cutter.sh
+# and https://stopsopa.github.io/pages/bash/index.html#cutter_sh
+# thanks to this heredoc fed to bash we can run all of this more than one time in a row 
+# and it will not override var/cutter_sh
 
-#    find . \
-#    -type f \
-#    \( -name '*.ts' -o -name '*.js' -o -name '*.tsx' \) \
-#    -not -path './.git/*' \
-#    -not -path './node_modules/*' \
-#    -exec awk '
-#        /__start__/ { found_start=1 }
-#        found_start && /__end__/ {
-#        print FILENAME
-#        exit
-#        }
-#    ' {} \; | /bin/bash bash/cutter.sh cut __start__ __end__
-
+# cat <<'EOF' | /bin/bash
 #
+# set -e
+# set -o pipefail
+#
+# START="__start__"
+# END="__end__"
+#
+# tmp="$(mktemp)"
+#
+# find . \
+# -type f \
+# \( -name '*.ts' -o -name '*.js' -o -name '*.tsx' \) \
+# -not -path './.git/*' \
+# -not -path './node_modules/*' \
+# -exec awk '
+#     /'"${START}"'/ { found_start=1 }
+#     found_start && /'"${END}"'/ {
+#     print FILENAME
+#     exit
+#     }
+# ' {} \; > "${tmp}"
+#
+# if [[ -s "${tmp}" ]]; then
+# mv "${tmp}" var/cutter_sh
+# /bin/bash bash/cutter.sh cut "${START}" "${END}" < var/cutter_sh
+# else
+# rm "${tmp}"
+# echo "bash/cutter.sh: Nothing to cut."
+# fi
+#
+# EOF
+
 # CUT:
 #   - Creates a backup: file -> file.cp
 #   - Removes all lines between START and END markers (including markers)
 #
 # Example:
 #
-#   // __start__
-#   import { Sandbox } from "./sandbox/Sandbox";
-#   // __end__
+#      // __start__
+#      import { Sandbox } from "./sandbox/Sandbox";
+#      // __end__
+# or
+#      {/* // __start__ */} 
+#      <Route path="/" element={<MainPage />} />
+#      {/* // __end__ */}
 #
 # becomes removed completely.
 #
-#
 # RESTORE:
 #
-#   find . -type f | /bin/bash bash/cutter.sh restore __start__ __end__
+#   cat var/cutter_sh | /bin/bash bash/cutter.sh restore __start__ __end__
 #
 #   Restores files from their .cp backup.
 #
@@ -59,6 +84,7 @@ END="${3}"
 
 if [[ -z "${ACTION}" || -z "${START}" || -z "${END}" ]]; then
   cat <<EOF
+${0}:  
 Usage:
   find . -type f | ${0} cut START END
   find . -type f | ${0} restore START END
@@ -74,7 +100,7 @@ while IFS= read -r file; do
       backup="${file}.cp"
 
       if [[ -e "${backup}" ]]; then
-        echo "Skipping ${file} (backup already exists)"
+        echo "${0}: Skipping ${file} (backup already exists)"
         continue
       fi
 
@@ -97,24 +123,25 @@ while IFS= read -r file; do
 
       mv "${file}.tmp" "${file}"
 
-      echo "Cut: ${file}"
+      echo "${0} processing: ${file}"
       ;;
 
     restore)
       backup="${file}.cp"
 
       if [[ ! -e "${backup}" ]]; then
-        echo "No backup for ${file}"
+        echo "${0}: No backup for ${file}"
         continue
       fi
 
       mv "${backup}" "${file}"
 
-      echo "Restored: ${file}"
+      echo "${0} restored: ${file}"
       ;;
 
     *)
       cat <<EOF
+${0}:      
 Unknown action: ${ACTION}
 
 Usage:
